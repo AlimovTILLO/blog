0b6dfe9de8c84712b29d8de2a86eab8c58ab51fa
AlimovTILLO <tillo.kg@mail.ru>
2015-10-14 09:59:51 +0600

    third commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,20 @@ class PostsController < ApplicationController
 
 
 	def new
+	end
+
+	def create
+		@post = Post.new (post_params)
+		@post.save
+
+		redirect_to @post
 	end	
+
+	def show
+		@post = Post.find(params[:id])
+	end
+	private
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
 end
